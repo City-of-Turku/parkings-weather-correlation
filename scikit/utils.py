7daa8b7c29a14aecf12b981148bd9da29898824e
Jokiniemi Juuso <juuso.jokiniemi@turku.fi>
2025-04-16 09:52:26 +0300

    Recaftor functionality to utils.py

--- scikit/utils.py
+++ scikit/utils.py
@@ -285,3 +285,23 @@ def get_diffs(prediction: pd.DataFrame, actual: pd.DataFrame) -> Tuple[pd.DataFr
     average_diff = df_diff["diff_to_pred_abs"].mean()
     return df_diff, average_percent_diff, average_diff
 
+
+def get_actual_hourly_parkings(df_prediction: pd.DataFrame):
+    df_actual = get_parkings_df("../data/pysakoinnit_2025_02_7-2025_03_09.csv")
+    df_zone1 = get_parkings_in_zone(df_actual, zone=1)
+    df_hourly_actual = get_hourly_parkings(df_zone1, time_end=pd.Timestamp("2025-03-09 11:00:00", tz="UTC"))
+    df_hourly_actual.index=df_prediction.index  
+    return df_hourly_actual
+
+
+def compare_prediction_with_actual(df_prediction: pd.DataFrame):
+    df_hourly_actual = get_actual_hourly_parkings(df_prediction)
+    df_diff, average_percent_diff , average_diff  = get_diffs(df_prediction, df_hourly_actual)
+    print("Average percent difference: ", average_percent_diff)
+    print("Average difference: ", average_diff)
+    df_results = pd.DataFrame(index=df_prediction.index)
+    df_results["prediction_num_parkings"] = df_prediction["num_parkings"]
+    df_results["actual_num_parkings"] = df_hourly_actual["num_parkings"]
+    # Sundays are not forecasted correctly
+    draw_diff(df_results)
+    draw_diff(df_diff)
